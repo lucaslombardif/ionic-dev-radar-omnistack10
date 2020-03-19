6141f35888060daeb9d611708af1efebad9026af
lucaslombardif <lucaas-floriano@hotmail.com>
2020-03-19 16:08:11 -0300

    Primeiro commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
+
+declare var google;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  map: any;
+  public devs: any = [];
+  public currentRegion: {
+    latitude,
+    longitude
+  }
+
+  public usernames: any = [];
+  public techs: string = 'Ionic';
+
+  constructor(public navCtrl: NavController, public api: ApiProvider) { }
+
+  async ionViewDidLoad() {
+
+    this.devs = await this.api.getDevs();
+    
+    for (let x in this.devs) { 
+      this.usernames.push(this.devs[x].github_username);
+    }
+
+    console.log(this.usernames);
+
+    const position = new google.maps.LatLng(-27.210794819883088, -49.63724979182513);
+
+    const mapOptions = {
+      zoom: 15,
+      center: position,
+      disableDefaultUI: true
+    }
+
+    this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    const marker = new google.maps.Marker({
+      position: position,
+      map: this.map,
+      //Titulo
+      title: 'Minha posição',
+      //Animção
+      animation: google.maps.Animation.DROP, // BOUNCE
+    });
+  }
+
+}
---
+++ src/providers/api/api.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ApiProvider {
+
+  public apiURL = 'http://localhost:3333';
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ApiProvider Provider');
+  }
+
+  public async getDevs() {
+    let devs = await this.http.get(this.apiURL + '/devs').toPromise();
+    return devs;
+  }
+
+}
