885dc38323c4934f8457ea3a6783d17203ef6ad6
lucaslombardif <lucaas-floriano@hotmail.com>
2020-03-22 21:08:05 -0300

    Adicionando Markers de acordo com pesquisa

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,45 +12,35 @@ declare var google;
 export class HomePage {
   map: any;
   public devs: any = [];
-  public locations: any = [];
   public avatars: any = [];
-  techs:any = 'HTML';
+  techs: any = 'HTML';
 
   constructor(public navCtrl: NavController, public api: ApiProvider) { }
-
-  public async searchDevs(){
-    this.devs = await this.api.searchDevs(-27.2111041, -49.6457925, this.techs) || []
-    console.log(this.devs);
-  }
-  
 
   async ionViewDidEnter() {
     let icons;
 
-    this.devs = await this.api.searchDevs(-27.2111041, -49.6457925, 'ReactJS') || [];
-
-    console.log(this.techs);
+    this.devs = await this.api.searchDevs(-27.2111041, -49.6457925, this.techs || []);
 
     for (let x in this.devs) {
       this.avatars.push(this.devs[x].avatar_url);
-      this.locations.push(this.devs[x].location.coordinates);
     };
 
     console.log(this.devs);
 
-    const position = new google.maps.LatLng(this.locations[0][1], this.locations[0][0]);
+    const position = new google.maps.LatLng(-27.2111041, -49.6457925);
 
     const mapOptions = {
-      zoom: 14,
+      zoom: 13,
       center: position,
       disableDefaultUI: true
     }
 
     this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    for (let i in this.locations) {
+    for (let i in this.devs) {
       const marker = new google.maps.Marker({
-        position: new google.maps.LatLng(this.locations[i][1], this.locations[i][0]),
+        position: new google.maps.LatLng(this.devs[i].location.coordinates[1], this.devs[i].location.coordinates[0]),
         map: this.map,
         icon: icons,
         scaledSize: new google.maps.Size(10, 10),
